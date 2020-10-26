7c21823990415548139c7ef4482d6b6cb888cd45
houssammahdi <houssam_mahdi_98@hotmail.com>
2020-10-26 14:06:05 +0100

    Updated Angular, TS-fixes

--- src/app/identify/identify.component.ts
+++ src/app/identify/identify.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-train',
   templateUrl: './identify.component.html',
-  styleUrls: ['./identify.component.css']
+  styleUrls: ['./identify.component.css'],
 })
 export class IdentifyComponent implements OnInit {
-
   uploadedFiles = [];
   faceIds: string[] = [];
   url: any;
@@ -16,56 +14,65 @@ export class IdentifyComponent implements OnInit {
   matchId: string;
   matchData: string;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  async handleFileInput(event, group_id: string){
+  async handleFileInput(event, group_id: string) {
     this.uploadedFiles = event.target.files;
     this.getPreview(this.uploadedFiles[0]);
-//    const tmp = await this.data.detectFace_File(this.uploadedFiles[0]).toPromise();
+    //    const tmp = await this.data.detectFace_File(this.uploadedFiles[0]).toPromise();
 
-    const getIDs = await this.data.detectFace_File(this.uploadedFiles[0]).toPromise().then(data => 
-      { 
-        let ressource = data[0];
-        this.faceIds[0] = ressource["faceId"];
-      }, 
-      error => console.log(error));
+    const getIDs = await this.data
+      .detectFace_File(this.uploadedFiles[0])
+      .toPromise()
+      .then(
+        (data) => {
+          const ressource = data[0];
+          this.faceIds[0] = ressource['faceId'];
+        },
+        (error) => console.log(error)
+      );
     this.faceIdentify(group_id);
   }
 
-  async faceIdentify(group_id: string){
+  async faceIdentify(group_id: string) {
+    const getID = await this.data
+      .faceIdentify(group_id, this.faceIds)
+      .toPromise()
+      .then((data) => (this.matchId = data.body[0].candidates[0].personId));
 
-    const getID = await this.data.faceIdentify(group_id, this.faceIds).toPromise().then(data => this.matchId = data.body[0].candidates[0].personId);
-  
-    const getName = await this.data.getPerson(group_id, this.matchId).toPromise().then(data =>
-      {
-        this.matchName = data.body["name"] 
-        this.matchData = data.body["userData"]
+    const getName = await this.data
+      .getPerson(group_id, this.matchId)
+      .toPromise()
+      .then((data) => {
+        this.matchName = data.body['name'];
+        this.matchData = data.body['userData'];
       });
   }
 
-
-  async handleUrlInput(url: string, group_id: string){
+  async handleUrlInput(url: string, group_id: string) {
     this.url = url;
-    const getIDs = await this.data.detectFace_URL(url).toPromise().then(data => 
-      { 
-        let ressource = data[0];
-        this.faceIds[0] = ressource["faceId"];
-      }, 
-      error => console.log(error));
+    const getIDs = await this.data
+      .detectFace_URL(url)
+      .toPromise()
+      .then(
+        (data) => {
+          const ressource = data[0];
+          this.faceIds[0] = ressource['faceId'];
+        },
+        (error) => console.log(error)
+      );
     this.faceIdentify(group_id);
   }
 
+  getPreview(file: File) {
+    const reader = new FileReader();
 
-  getPreview(file: File){
-    var reader = new FileReader();
-  
     reader.readAsDataURL(file);
-  
-    reader.onload = (event) => { 
-    this.url = (<FileReader>event.target).result;
-    } 
+
+    reader.onload = (event) => {
+      this.url = (event.target as FileReader).result;
+    };
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DataService {
-
   response: any = [];
   private endpoint_url = 'https://westeurope.api.cognitive.microsoft.com';
-  private detect_url = this.endpoint_url + '/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise';
+  private detect_url =
+    this.endpoint_url +
+    '/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise';
   private person_groups_url = this.endpoint_url + '/face/v1.0/persongroups/';
   private face_identify_url = this.endpoint_url + '/face/v1.0/identify';
-  private subscriptionKey: string = '9a70ba47f8b1453694c2b107fd69bb14';
+  private subscriptionKey = '9a70ba47f8b1453694c2b107fd69bb14';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private get_Image_Upload_Headers() {
     let headers = new HttpHeaders();
@@ -25,48 +25,96 @@ export class DataService {
     return headers;
   }
 
-  private getHeaders(){
+  private getHeaders() {
     let headers = new HttpHeaders();
     headers = headers.set('Ocp-Apim-Subscription-Key', this.subscriptionKey);
     headers = headers.set('Content-Type', 'application/json');
     return headers;
   }
 
-  detectFace_File(image: File){
-    return this.http.post<any>(this.detect_url, image, { headers: this.get_Image_Upload_Headers()});
+  detectFace_File(image: File) {
+    return this.http.post<any>(this.detect_url, image, {
+      headers: this.get_Image_Upload_Headers(),
+    });
   }
 
-  detectFace_URL(imageURL: string){
+  detectFace_URL(imageURL: string) {
     console.log(imageURL);
-    return this.http.post<any>(this.detect_url, {url: imageURL}, { headers: this.getHeaders()});
+    return this.http.post<any>(
+      this.detect_url,
+      { url: imageURL },
+      { headers: this.getHeaders() }
+    );
   }
 
-  createPersonGroup(group_name: string, _name: string, _userData: string){
-    return this.http.put<any>(this.person_groups_url + group_name, {name: _name, userData: _userData}, { headers: this.getHeaders(), responseType: 'json', observe: 'response'});
+  createPersonGroup(group_name: string, _name: string, _userData: string) {
+    return this.http.put<any>(
+      this.person_groups_url + group_name,
+      { name: _name, userData: _userData },
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
   }
 
-  createPerson(group_id:string, _name: string, _userData: string){
-    return this.http.post<any>(this.person_groups_url + group_id + '/persons/', {name: _name, userData: _userData}, { headers: this.getHeaders(), responseType: 'json', observe: 'response'});
+  createPerson(group_id: string, _name: string, _userData: string) {
+    return this.http.post<any>(
+      this.person_groups_url + group_id + '/persons/',
+      { name: _name, userData: _userData },
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
   }
 
-  addPersonImage(group_id: string, image: File, personID: string){
-    return this.http.post<any>(this.person_groups_url + group_id + '/persons/' + personID + '/persistedFaces', image, { headers: this.get_Image_Upload_Headers(), responseType: 'json', observe:'response'});
+  addPersonImage(group_id: string, image: File, personID: string) {
+    return this.http.post<any>(
+      this.person_groups_url +
+        group_id +
+        '/persons/' +
+        personID +
+        '/persistedFaces',
+      image,
+      {
+        headers: this.get_Image_Upload_Headers(),
+        responseType: 'json',
+        observe: 'response',
+      }
+    );
   }
 
-  getPerson(group_id: string, _personID: string){
-    return this.http.get<any>(this.person_groups_url + group_id + '/persons/' + _personID, { headers: this.getHeaders(), responseType: 'json', observe: 'response'});
-  } 
-
-  getPersonFace(_personGroupID: string, _personID: string, _persistedFaceID: string){
-    return this.http.get<any>(this.person_groups_url + _personGroupID + '/persons/' + _personID + '/persistedFaces/' + _persistedFaceID, { headers: this.getHeaders(), responseType: 'json', observe: 'response'});
+  getPerson(group_id: string, _personID: string) {
+    return this.http.get<any>(
+      this.person_groups_url + group_id + '/persons/' + _personID,
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
   }
 
-  trainGroup(group_id: string){
-    return this.http.post<any>(this.person_groups_url + group_id + '/train', {}, { headers: this.getHeaders(), responseType: 'json', observe: 'response'});
+  getPersonFace(
+    _personGroupID: string,
+    _personID: string,
+    _persistedFaceID: string
+  ) {
+    return this.http.get<any>(
+      this.person_groups_url +
+        _personGroupID +
+        '/persons/' +
+        _personID +
+        '/persistedFaces/' +
+        _persistedFaceID,
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
   }
 
-  faceIdentify(group_id: string, _faceIds: string[]){
-    return this.http.post<any>(this.face_identify_url, { faceIds: _faceIds, personGroupId: group_id }, { headers: this.getHeaders(), responseType: 'json', observe: 'response' });
+  trainGroup(group_id: string) {
+    return this.http.post<any>(
+      this.person_groups_url + group_id + '/train',
+      {},
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
+  }
+
+  faceIdentify(group_id: string, _faceIds: string[]) {
+    return this.http.post<any>(
+      this.face_identify_url,
+      { faceIds: _faceIds, personGroupId: group_id },
+      { headers: this.getHeaders(), responseType: 'json', observe: 'response' }
+    );
   }
 }
-
